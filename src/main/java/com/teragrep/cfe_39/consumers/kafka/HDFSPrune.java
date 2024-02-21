0426a783324b3fc552055842389ab7e0dc0acf5f
Tiihott <48@teragrep.com>
2024-02-21 08:26:04 +0200

    Added the missing kerberized access to HDFSPrune.java.

--- src/main/java/com/teragrep/cfe_39/consumers/kafka/HDFSPrune.java
+++ src/main/java/com/teragrep/cfe_39/consumers/kafka/HDFSPrune.java
@@ -21,12 +21,16 @@ import com.teragrep.cfe_39.Config;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class HDFSPrune {
@@ -35,32 +39,85 @@ public class HDFSPrune {
     private final FileSystem fs;
     private Path newFolderPath;
     private long cutoff_epoch;
+    private final boolean useMockKafkaConsumer; // test-mode switch
 
     public HDFSPrune(Config config, String topicName) throws IOException {
-        this.config = config;
-        String hdfsuri = config.getHdfsuri();
 
-        String path = config.getHdfsPath()+"/"+topicName;
-        // ====== Init HDFS File System Object
-        Configuration conf = new Configuration();
-        // Set FileSystem URI
-        conf.set("fs.defaultFS", hdfsuri);
-        // Because of Maven
-        conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
-        conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-        // Set HADOOP user
-        System.setProperty("HADOOP_USER_NAME", "hdfs");
-        System.setProperty("hadoop.home.dir", "/");
-        //Get the filesystem - HDFS
-        fs = FileSystem.get(URI.create(hdfsuri), conf);
+        // Check for testmode from config.
+        Properties readerKafkaProperties = config.getKafkaConsumerProperties();
+        this.useMockKafkaConsumer = Boolean.parseBoolean(
+                readerKafkaProperties.getProperty("useMockKafkaConsumer", "false")
+        );
 
-        //==== Create folder if not exists
-        Path workingDir=fs.getWorkingDirectory();
-        newFolderPath= new Path(path);
-        if(!fs.exists(newFolderPath)) {
-            // Create new Directory
-            fs.mkdirs(newFolderPath);
-            LOGGER.info("Path "+path+" created.");
+        if (useMockKafkaConsumer) {
+            this.config = config;
+            String hdfsuri = config.getHdfsuri();
+
+            String path = config.getHdfsPath() + "/" + topicName;
+            // ====== Init HDFS File System Object
+            Configuration conf = new Configuration();
+            // Set FileSystem URI
+            conf.set("fs.defaultFS", hdfsuri);
+            // Because of Maven
+            conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+            conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
+            // Set HADOOP user
+            System.setProperty("HADOOP_USER_NAME", "hdfs");
+            System.setProperty("hadoop.home.dir", "/");
+            //Get the filesystem - HDFS
+            fs = FileSystem.get(URI.create(hdfsuri), conf);
+
+            //==== Create folder if not exists
+            Path workingDir = fs.getWorkingDirectory();
+            newFolderPath = new Path(path);
+            if (!fs.exists(newFolderPath)) {
+                // Create new Directory
+                fs.mkdirs(newFolderPath);
+                LOGGER.info("Path " + path + " created.");
+            }
+        }else {
+            // Code for initializing the class with kerberos.
+            String hdfsuri = config.getHdfsuri(); // Get from config.
+
+            String path = config.getHdfsPath() + "/" + topicName;
+
+            // set kerberos host and realm
+            System.setProperty("java.security.krb5.realm", config.getKerberosRealm());
+            System.setProperty("java.security.krb5.kdc", config.getKerberosHost());
+
+            Configuration conf = new Configuration();
+
+            // enable kerberus
+            conf.set("hadoop.security.authentication", config.getHadoopAuthentication());
+            conf.set("hadoop.security.authorization", config.getHadoopAuthorization());
+
+            conf.set("fs.defaultFS", hdfsuri); // Set FileSystem URI
+            conf.set("fs.hdfs.impl", DistributedFileSystem.class.getName()); // Maven stuff?
+            conf.set("fs.file.impl", LocalFileSystem.class.getName()); // Maven stuff?
+
+            // hack for running locally with fake DNS records
+            // set this to true if overriding the host name in /etc/hosts
+            conf.set("dfs.client.use.datanode.hostname", config.getKerberosTestMode());
+
+            // server principal
+            // the kerberos principle that the namenode is using
+            conf.set("dfs.namenode.kerberos.principal.pattern", config.getKerberosPrincipal());
+
+            // set usergroup stuff
+            UserGroupInformation.setConfiguration(conf);
+            UserGroupInformation.loginUserFromKeytab(config.getKerberosKeytabUser(), config.getKerberosKeytabPath());
+
+            // filesystem for HDFS access is set here
+            fs = FileSystem.get(conf);
+
+            //==== Create folder if not exists
+            Path workingDir = fs.getWorkingDirectory();
+            newFolderPath = new Path(path);
+            if (!fs.exists(newFolderPath)) {
+                // Create new Directory
+                fs.mkdirs(newFolderPath);
+                LOGGER.info("Path " + path + " created.");
+            }
         }
         long pruneOffset = config.getPrune_offset();
         cutoff_epoch = System.currentTimeMillis() - pruneOffset; // pruneOffset is parametrized in Config.java. Default value is 2 days in milliseconds.
